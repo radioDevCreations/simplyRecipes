8f710278a4f71100096ffc28b5bb344e01d51f60
radioDevCreations <radekrozmus@gmail.com>
2021-09-03 18:48:44 +0200

    working version

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
-      resolve: `gatsby
-      
-      -source-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
